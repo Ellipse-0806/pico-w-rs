84d6ad2d4258152907a3709606d855418b8b2519
RyKato <kato980005@gmail.com>
2024-03-01 15:43:13 +0900

    blinkとファームウェアなど追加

---
+++ blink/src/main.rs
@@ -0,0 +1,67 @@
+#![no_std]
+#![no_main]
+
+use cyw43_pio::PioSpi;
+use defmt::unwrap;
+use defmt_rtt as _; // rust-lld: error: undefined symbol: _defmt_acquire
+use embassy_executor::Spawner;
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::Output;
+use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_time::{Duration, Timer};
+use panic_probe as _;
+use static_cell::StaticCell;
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
+#[embassy_executor::task]
+async fn wifi_task(
+    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
+) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let fw = include_bytes!("../../cyw43-firmware/43439A0.bin");
+    let clm = include_bytes!("../../cyw43-firmware/43439A0_clm.bin");
+
+    let pwr = Output::new(p.PIN_23, embassy_rp::gpio::Level::Low);
+    let cs = Output::new(p.PIN_25, embassy_rp::gpio::Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
+
+    // argument requires that `state` is borrowed for `'static`
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+
+    let (_new_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    unwrap!(spawner.spawn(wifi_task(runner)));
+
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+
+    let delay = Duration::from_secs(1);
+    loop {
+        control.gpio_set(0, true).await;
+        Timer::after(delay).await;
+
+        control.gpio_set(0, false).await;
+        Timer::after(delay).await;
+    }
+}
